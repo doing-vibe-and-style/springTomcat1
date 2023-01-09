9f086616a7b685ae4d7c8c3954052934a0b5e21b
Rustam Fatkhullin <qwerus99@yandex.ru>
2023-01-09 20:51:26 +0600

    2nd commit

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,5 +1,6 @@
 package web.model;
 
+
 public class Car {
     String model;
     int series;
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,31 +1,20 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.DAO.CarDAO;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Service
 public class CarServiceImpl implements CarService{
-    List<Car> carList;
 
-    {
-        carList = new ArrayList<>();
-        Car car1 = new Car("car1", 1, "a1a1a1");
-        Car car2 = new Car("car2", 2, "a2a1a1");
-        Car car3 = new Car("car3", 3, "a3a1a1");
-        Car car4 = new Car("car4", 4, "a4a1a1");
-        Car car5 = new Car("car5", 5, "a5a1a1");
-        carList.add(car1);
-        carList.add(car2);
-        carList.add(car3);
-        carList.add(car4);
-        carList.add(car5);
-    }
+    @Autowired
+    private CarDAO carDAO;
 
     @Override
     public List<Car> filterCars(int i) {
-        return carList.stream().limit(i).toList();
+        return carDAO.filterCars(i);
     }
 }
